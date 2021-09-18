e59c2989303651cdd564409c652af5ef81d0794c
alienvictor2000 <nnguyenduchuy2000@gmail.com>
2021-09-18 21:23:47 +0700

    final

--- Modules/Admin/Resources/views/category/form.blade.php
+++ Modules/Admin/Resources/views/category/form.blade.php
@@ -9,18 +9,5 @@
             </span>
         @endif
     </div>
-{{--    <div class="form-group">--}}
-{{--        <label for="name">Meta Title:</label>--}}
-{{--        <input type="text" class="form-control" placeholder="Meta title" value="{{ old('c_title_seo',isset($category->c_title_seo) ? $category->c_title_seo : '') }}" name="c_title_seo">--}}
-{{--    </div>--}}
-{{--    <div class="form-group">--}}
-{{--        <label for="name">Meta Description</label>--}}
-{{--        <input type="text" class="form-control" placeholder="Meta Description" value="{{ old('c_description_seo',isset($category->c_description_seo) ? $category->c_description_seo  : '') }}" name="c_description_seo">--}}
-{{--    </div>--}}
-{{--    <div class="form-group">--}}
-{{--        <div class="checkbox">--}}
-{{--            <label><input type="checkbox" name="hot">Nổi bật</label>--}}
-{{--        </div>--}}
-{{--    </div>--}}
     <button type="submit" class="btn btn-success">Lưu thông tin</button>
 </form>
